2277d15d885abed2cdc43b631a2b73b988f5a086
Bennett Joerger <bennettjoerger@gmail.com>
2016-05-31 17:48:17 -0600

    update

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-$(document).ready(function)(){
-  console.log("Sanity check");
-}
+$(document).ready(function (){
+  console.log("Sanity check")
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var movieName = $('#moviesearch').val();
+    $.get('https://galvanize-cors-proxy.herokuapp.com/https://api.themoviedb.org/3/search/movie/?api_key=69d2ced7c1460e81b5b6e63b73efe5f2&query=' + movieName,function(data) {
+        var results = data.results
+        for (var i = 0; i < results.length; i++){
+          $('.shit').html('<p>'+ results[i].overview + '</p>')
+        }
+  })
+})
+
+
+});
